9ee6a49fe6b59002ab7e8d453cf0aa897f8d7d3a
yrobin <yoann.robin.k@gmail.com>
2023-09-06 10:14:35 +0200

    Version 1.3.0: remove NetworkSimplex c++ code and add pot package as solver

--- SBCK/__release.py
+++ SBCK/__release.py
@@ -18,7 +18,7 @@
 
 
 version_major = 1
-version_minor = 2
+version_minor = 3
 version_patch = 0
 version_extra = ""
 version       = "{}.{}.{}{}".format(version_major,version_minor,version_patch,version_extra)

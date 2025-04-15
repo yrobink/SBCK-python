063011bc6f93b00b00a7b4f2479d68e468ef9537
yrobin <yoann.robin.k@gmail.com>
2025-04-15 15:07:19 +0200

    Update

--- SBCK/ppp/__MomentsBC.py
+++ SBCK/ppp/__MomentsBC.py
@@ -39,9 +39,11 @@ class UNormalAdjust(PrePostProcessing):##{{{
 	standard deviation are corrected at the end.
 	
 	"""
-	def __init__( self , *args , **kwargs ):##{{{
+	def __init__( self , *args , recs_Xt = False , add_Xt1_bias = False , **kwargs ):##{{{
 		PrePostProcessing.__init__( self , *args , **kwargs )
-		self._name      = "UNormalAdjust"
+		self._name    = "UNormalAdjust"
+		self._recs_Xt = recs_Xt
+		self._add_Xt1_bias = add_Xt1_bias
 		
 		self._m  = { "Y0" : 0 , "X0" : 0 , "X1" : 0 }
 		self._C  = { "Y0" : 1 , "X0" : 1 , "X1" : 1 }
@@ -72,6 +74,19 @@ class UNormalAdjust(PrePostProcessing):##{{{
 	def itransform( self , Xt ):##{{{
 		k = self._kind
 		
+		if self._recs_Xt:
+			mXt  = Xt.mean( axis = 0 ).reshape(-1,1)
+			sXt  = Xt.std(  axis = 0 )
+			isXt = 1. / sXt
+			sXt  = np.diag( sXt)
+			isXt = np.diag(isXt)
+			Xt = ( isXt @ (Xt.T - mXt) ).T
+		
+		S = np.identity(Xt.shape[1])
+		M = np.zeros( (Xt.shape[1],1) )
+		if self._add_Xt1_bias:
+			pass
+		
 		match k:
 			case "Y0":
 				X = self._s[k] @ Xt.T + self._m[k]
@@ -79,9 +94,8 @@ class UNormalAdjust(PrePostProcessing):##{{{
 				k0 = "Y0"
 				X = self._s[k0] @ Xt.T + self._m[k0]
 			case "X1":
-				cF  = self._s["Y0"] @ self._is["X0"]
-				D01 = cF @ (self._m['X1'] - self._m['X0'])
-				X   = self._s['X1'] @ self._is['X0'] @ self._s['Y0'] @ Xt.T + D01 + self._m['Y0']
+				D01 = self._s["Y0"] @ self._is["X0"] @ (self._m['X1'] - self._m['X0'])
+				X   = S @ self._s['X1'] @ self._is['X0'] @ self._s['Y0'] @ Xt.T + D01 + self._m['Y0'] + M
 		
 		return X.T
 		##}}}
@@ -99,9 +113,10 @@ class MNormalAdjust(PrePostProcessing):##{{{
 	
 	"""
 	
-	def __init__( self , *args , **kwargs ):##{{{
+	def __init__( self , *args , recs_Xt = False , **kwargs ):##{{{
 		PrePostProcessing.__init__( self , *args , **kwargs )
-		self._name      = "MNormalAdjust"
+		self._name    = "MNormalAdjust"
+		self._recs_Xt = recs_Xt
 		
 		self._m  = { "Y0" : 0 , "X0" : 0 , "X1" : 0 }
 		self._C  = { "Y0" : 1 , "X0" : 1 , "X1" : 1 }
@@ -133,6 +148,13 @@ class MNormalAdjust(PrePostProcessing):##{{{
 	
 	def itransform( self , Xt ):##{{{
 		k = self._kind
+		
+		if self._recs_Xt:
+			mXt  = Xt.mean( axis = 0 ).reshape(-1,1)
+			C  = np.cov(Xt.T)
+			S  = sqrtm(C)
+			iS = np.linalg.pinv(S)
+			Xt = ( iS @ (Xt.T - mXt) ).T
 		
 		match k:
 			case "Y0":
